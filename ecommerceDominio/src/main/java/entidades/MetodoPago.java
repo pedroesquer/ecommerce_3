0dfeb007c4dcb4f5fe2c59e64037bda13514dac2
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-10-15 21:10:25 -0700

    feat: Mayoria de entidades ya hechas

---
+++ ecommerceDominio/src/main/java/entidades/MetodoPago.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author pedro
+ */
+public class MetodoPago {
+    private double monto;
+    private LocalDateTime fecha;
+    private String estado;
+
+    public MetodoPago(double monto, LocalDateTime fecha, String estado) {
+        this.monto = monto;
+        this.fecha = fecha;
+        this.estado = estado;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
---
+++ ecommerceDominio/src/main/java/entidades/ReseñaProducto.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author pedro
+ */
+public class ReseñaProducto {
+    private int estrellas;
+    private String comentario;
+    private LocalDate fecha;
+    private Usuario usuario;
+
+    public ReseñaProducto(int estrellas, String comentario, LocalDate fecha, Usuario usuario) {
+        this.estrellas = estrellas;
+        this.comentario = comentario;
+        this.fecha = fecha;
+        this.usuario = usuario;
+    }
+
+    public int getEstrellas() {
+        return estrellas;
+    }
+
+    public void setEstrellas(int estrellas) {
+        this.estrellas = estrellas;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    
+    
+}
