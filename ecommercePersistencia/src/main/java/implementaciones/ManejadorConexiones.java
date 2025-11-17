7d6a41b979e6942957e5533ecc1b577740327701
gaelguerra <gaelguerra05@icloud.com>
2025-11-16 18:36:29 -0700

    Interface package created
    Methods requiered for advance 3 were established.

--- ecommercePersistencia/src/main/java/implementaciones/ManejadorConexiones.java
+++ ecommercePersistencia/src/main/java/implementaciones/ManejadorConexiones.java
@@ -4,10 +4,19 @@
  */
 package implementaciones;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author gael_
  */
 public class ManejadorConexiones {
-    
+    public static EntityManager getEntityManager() {
+        EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("com.mycompany_ecommercePersistencia_jar_1.0-SNAPSHOTPU");
+        EntityManager entityManager = emFactory.createEntityManager();
+        return entityManager;
+
+    }
 }
---
+++ ecommercePersistencia/src/main/java/interfaces/IProductosDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+/**
+ *
+ * @author gael_
+ */
+public interface IProductosDAO {
+    public void eliminarProducto();
+    public void agregarProducto(); 
+    public void editarProducto(); 
+}
